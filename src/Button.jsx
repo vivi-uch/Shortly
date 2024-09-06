6344ddfe4633c1b6610d5de3d6dd2d4770c27dfc
vivi-uch <vivianokonkwouch>
2024-09-06 16:51:00 +0100

    navigation

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ name, styling }) {
   return (
     <div
-      className={`flex px-6 py-2 bg-green-900 text-white font-bold text-[14px] md:text-[16px] ${styling}`}
+      className={`flex px-6 py-2 bg-green-900 text-white hover:bg-green-500 justify-center  font-bold text-[14px] md:text-[16px] ${styling}`}
     >
       {name}
     </div>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,20 +1,93 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [active, setActive] = useState("features");
+
   return (
-    <div className="flex justify-between items-center py-2  md:my-6 px-4 md:px-32 text-xs md:text-[18px]">
-      <div className="flex items-center gap-5">
-        <h1 className="text-[2rem] md:text-4xl font-bold">Shortly</h1>
-        <div className="hidden text-slate-400 md:flex gap-5 ml-4">
-          <p>Features</p>
-          <p>Pricing</p>
-          <p>Resources</p>
+    <div className="flex items-center py-2 md:my-6 px-4 md:px-32 text-xs md:text-[18px]">
+      <img
+        src="images/logo.svg"
+        alt="logo"
+        className="mr-12 w-24 md:w-32 lg:w-40"
+      />
+
+      {/* {laptop view} */}
+      <div className="hidden md:flex justify-between flex-grow">
+        <div className="text-slate-400 gap-8 flex justify-center items-center text-lg">
+          <a
+            href="#features"
+            className={`${
+              active === "features" ? "border-b-2 border-Dark-Violet" : ""
+            } hover:text-gray-900`}
+            onClick={() => setActive("features")}
+          >
+            Features
+          </a>
+          <a
+            href="#pricing"
+            className={`${
+              active === "pricing" ? "border-b-2 border-Dark-Violet" : ""
+            } hover:text-gray-900`}
+            onClick={() => setActive("pricing")}
+          >
+            Pricing
+          </a>
+          <a
+            href="#resources"
+            className={`${
+              active === "resources" ? "border-b-2 border-Dark-Violet" : ""
+            } hover:text-gray-900`}
+            onClick={() => setActive("resources")}
+          >
+            Resources
+          </a>
+        </div>
+        <div className="flex items-center">
+          <p className="text-slate-400 mr-8 hover:text-gray-900">Login</p>
+          <Button name="Sign up" styling="rounded-[44px]" />
         </div>
       </div>
-      <div className="hidden md:flex items-center">
-        <p className="text-slate-400 mr-8">Login</p>
-        <Button name="Sign up" styling="rounded-[44px]" />
-      </div>
+
+      {/* {mobile view} */}
+      <button className="absolute right-4" onClick={() => setIsOpen(!isOpen)}>
+        <img
+          src="images/icon-menu.svg"
+          alt="menu-button"
+          className="lg:hidden h-6 w-8"
+        />
+      </button>
+
+      {isOpen && (
+        <div className="fixed inset-0 z-50">
+          <div className="absolute bg-purple-950  px-6 py-10 top-20 inset-6 rounded-md">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="absolute top-2 right-[1.5rem]"
+            >
+              ❌
+            </button>
+
+            <nav className="flex flex-col gap-5 justify-center text-center text-lg text-white font-semibold">
+              <a href="#home" className="hover:text-gray-600 ">
+                Features
+              </a>
+              <a href="#new" className="hover:text-gray-600 ">
+                Pricing
+              </a>
+              <a href="#popular" className="hover:text-gray-600 ">
+                Resources
+              </a>
+              <hr className="bg-slate-500 opacity-25 my-0" />
+              <a href="#trending" className="hover:text-gray-600 ">
+                Login
+              </a>
+              <Button name="Sign up" styling="rounded-[44px]" />
+            </nav>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
